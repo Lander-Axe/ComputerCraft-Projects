6bea1c24aae9a5ae9aa62ab097c78aa90b7c36eb
Lander-Axe <68823170+Lander-Axe@users.noreply.github.com>
2023-09-17 11:29:05 -0400

    Create drop_inventory.lua
    First version of drop_inventory.lua.

---
+++ drop_inventory.lua
@@ -0,0 +1,17 @@
+modules = peripheral.find("neuralInterface")
+
+storage = {modules.getBaubles(), modules.getEquipement(), modules.getInventory()}
+
+BAUBLES = 1
+EQUIPEMENT = 2
+INVENTORY = 3
+
+function drop() do
+    for i=1,storage.size() do
+        for ii=1,storage[i].size() do
+            storage[i][ii].drop()
+        end
+    end
+end
+
+drop()
