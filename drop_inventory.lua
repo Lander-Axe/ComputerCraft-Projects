aac2d6d91fc87ea7de8cf27279c977c7e33f7b1c
Lander-Axe <68823170+Lander-Axe@users.noreply.github.com>
2023-09-17 12:32:14 -0400

    Fixed nil value in drop_inventory.lua
    drop_inventory.lua now checks if the item is nil before trying to drop it.

--- drop_inventory.lua
+++ drop_inventory.lua
@@ -5,7 +5,10 @@ storage = {modules.getBaubles(), modules.getEquipment(), modules.getInventory()}
 function drop()
     for i=1,table.getn(storage) do
         for ii=1,storage[i].size() do
-            storage[i][ii].drop()
+            item = storage[i][ii]
+            if item ~= nil then
+                item.drop()
+            end
         end
     end
 end
