bcf11e07e220d5574767291c8424834894454f09
Lander-Axe <68823170+Lander-Axe@users.noreply.github.com>
2023-09-17 12:07:31 -0400

    Fixed function used in drop_inventory.lua
    Replaced len() by table.getn() because len() is also not valid for tables.

--- drop_inventory.lua
+++ drop_inventory.lua
@@ -3,7 +3,7 @@ modules = peripheral.find("neuralInterface")
 storage = {modules.getBaubles(), modules.getEquipment(), modules.getInventory()}
 
 function drop()
-    for i=1,len(storage) do
+    for i=1,table.getn(storage) do
         for ii=1,storage[i].size() do
             storage[i][ii].drop()
         end
